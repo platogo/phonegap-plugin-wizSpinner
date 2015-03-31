f681aec55f1656a4c4a62eab498e3fc7abbea318
Christoph Altmann <christoph.altmann@platogo.com>
2015-03-31 15:59:51 +0200

    Remove import com.platogo.pmp.R and get resources from main activity instead

--- android/jp/wizcorp/phonegap/plugin/wizSpinner/WizSpinner.java
+++ android/jp/wizcorp/phonegap/plugin/wizSpinner/WizSpinner.java
@@ -10,61 +10,63 @@ import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.view.animation.AnimationUtils;
 
-import com.platogo.pmp.R;
-
 public class WizSpinner {
-    
-    private static Dialog dialog;
-    private final static String TAG = "WizSpinner";
-    public static boolean isVisible = false;
-    
-    public static void show(Activity activity) {
-        // create and show the spinner
-        
-        final Activity _ctx = activity;
-        
-        if ( !isVisible ) {
-            Log.i(TAG, "[display spinner] ******* ");
-            
-            activity.runOnUiThread(
-                                   new Runnable() {
-                public void run() {
-                    dialog = new Dialog(_ctx);
-                    
-                    Window window = dialog.getWindow();
-                    window.setBackgroundDrawable(new ColorDrawable(0));
-                    
-                    WindowManager.LayoutParams wlp = window.getAttributes();
-                    wlp.gravity = Gravity.BOTTOM;
-                    wlp.y = 32;
-                    window.clearFlags(LayoutParams.FLAG_DIM_BEHIND);
-                    window.setAttributes(wlp);
-                    
-                    dialog.setContentView(R.layout.com_platogo_pmp_loading);
-                    dialog.setCancelable(false);
-                    dialog.show();
-                    dialog.findViewById(R.id.loading_spinner).startAnimation(AnimationUtils.loadAnimation(_ctx, R.anim.rotate_spinner));
-                }
-            }
-                                   );
-            
-            isVisible = true;
+
+  private static Dialog dialog;
+  private final static String TAG = "WizSpinner";
+  public static boolean isVisible = false;
+
+  public static void show(Activity activity) {
+    // create and show the spinner
+
+    final Activity _ctx = activity;
+
+    if ( !isVisible ) {
+      Log.i(TAG, "[display spinner] ******* ");
+
+      activity.runOnUiThread(
+        new Runnable() {
+          public void run() {
+            int layoutId = _ctx.getResources().getIdentifier("loading_spinner", "layout",_ctx.getApplicationContext().getPackageName());
+            int rotateSpinnerId = _ctx.getResources().getIdentifier("loading_spinner", "anim",_ctx.getApplicationContext().getPackageName());
+            int loadingSpinnerId = _ctx.getResources().getIdentifier("loading_spinner", "id",_ctx.getApplicationContext().getPackageName());
+
+            dialog = new Dialog(_ctx);
+
+            Window window = dialog.getWindow();
+            window.setBackgroundDrawable(new ColorDrawable(0));
+
+            WindowManager.LayoutParams wlp = window.getAttributes();
+            wlp.gravity = Gravity.BOTTOM;
+            wlp.y = 32;
+            window.clearFlags(LayoutParams.FLAG_DIM_BEHIND);
+            window.setAttributes(wlp);
+
+            dialog.setContentView(layoutId);
+            dialog.setCancelable(false);
+            dialog.show();
+            dialog.findViewById(loadingSpinnerId).startAnimation(AnimationUtils.loadAnimation(_ctx, rotateSpinnerId));
+          }
         }
+      );
+
+      isVisible = true;
     }
-    
-    public static void hide(Activity activity) {
-        // hide the spinner
-        if ( isVisible ) {
-            Log.i(TAG, "[Hiding spinner] ******* ");
-            
-            activity.runOnUiThread(
-                                   new Runnable() {
-                public void run() {
-                    dialog.dismiss();
-                }
-            }
-                                   );
-            isVisible = false;
+  }
+
+  public static void hide(Activity activity) {
+    // hide the spinner
+    if ( isVisible ) {
+      Log.i(TAG, "[Hiding spinner] ******* ");
+
+      activity.runOnUiThread(
+        new Runnable() {
+          public void run() {
+            dialog.dismiss();
+          }
         }
+      );
+      isVisible = false;
     }
+  }
 }
